d3a43c6575f0b21181c35d11327a96346e12bc35
Lukasz Smiechowski <lukasz.smiechowski@me.com>
2023-05-14 20:38:08 +0200

    nav mobile and desktop 2/2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
 'use strict'
 
+/////////////////////////////
+//Mobile navigation
+
 const btnNav = document.querySelector('.nav-mobile-btn');
 const navEl = document.querySelector('.nav');
+const menuOpen = document.querySelector('.nav-mobile-open');
+const menuClose = document.querySelector('.nav-mobile-close');
 
 btnNav.addEventListener('click', function () {
   navEl.classList.toggle('nav-open');
+  menuOpen.classList.toggle('.hidden');
+  menuClose.classList.toggle('.hidden');
+
 });
+
+//////////////////////////////
+//Sticky navigation
