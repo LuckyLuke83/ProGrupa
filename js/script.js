e76a3ea6d7642c0b21b65b7396b54171ea81c401
Lukasz Smiechowski <lukasz.smiechowski@me.com>
2023-05-14 20:55:32 +0200

    sticky nav

--- js/script.js
+++ js/script.js
@@ -17,3 +17,22 @@ btnNav.addEventListener('click', function () {
 
 //////////////////////////////
 //Sticky navigation
+
+const headerSection = document.querySelector('.header');
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) document.body.classList.add('sticky');
+
+    if (ent.isIntersecting) document.body.classList.remove('sticky');
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: '-20px',
+  }
+);
+
+obs.observe(headerSection);
